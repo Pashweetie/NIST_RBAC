d86fcfb89ab349486428eb7223bf5a4fec0e9b6f
Pashweetie <Pashweetie@gmail.com>
2019-11-05 19:45:31 -0700

    i got something going here?

--- program.py
+++ program.py
@@ -1,12 +1,15 @@
 class Node:
-  def __init__(self, role, descendants, ascendant):
+  def __init__(self, role = None, descendants=[], ascendant=[]):
     self.role = role
     self.descendants = descendants
     self.ascendant = ascendant
 def build_tree():
     ayy = 0
 def build_tree_test(root, role):
-    root.ascendant = Node(role,root,None)
+    print("before"+root.role)
+    root.ascendant.append(Node(role,root,None))
+    print("after"+root.role)
+    
     return root
 def main():
     while True:
@@ -17,10 +20,14 @@ def main():
             print('oops, please have a valid roleHierarchy.txt file in the directory, then press enter')
             input()
     fline = f.readlines()
+    root = Node()
     for line in fline:
         a_d_array = line.split()
-        print("ascendant"+str(a_d_array[0]))
-        print("descendant"+str(a_d_array[1]))
-
+        root.role = a_d_array[0]
+        root = build_tree_test(root,a_d_array[1])
+    print_tree(root)
+def print_tree(root):
+    for nodes in root.ascendant:
+        print(nodes.role)
 if __name__ == "__main__":
   main()
