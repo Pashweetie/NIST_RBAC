df111c1d2e183a53b4428c8bcfb75de1f32503af
Pashweetie <Pashweetie@gmail.com>
2019-11-06 21:42:46 -0700

    updoot

--- program.py
+++ program.py
@@ -7,13 +7,15 @@ def build_tree(a_descendant, a_ascendant):
     a_descendant.ascendant.append(Node(a_ascendant.role,a_descendant))
     return a_descendant
 def traverse_down(start,role):
+    # print('start')
+    # print_tree(start)
     if start.ascendant == []:
         return (None,False)
     elif start.role == role:
         return (start,True)
     else:
-        # print(start.role)
-        # print(role)
+        print(start.role)
+        print(role)
         # print(len(start.ascendant))
         for i in start.ascendant:
             # print(start.role)
@@ -37,30 +39,22 @@ def main():
     f.close()
     print_array(needToCheck)
     count = 0
-    while needToCheck != []:
+    while needToCheck != []:        
         for line in needToCheck:
             print('on line:')
             print(line)
-            print('root is')
-            print(root.role)
-            print()
-            print_array(needToCheck)
-            if count != 0:
-                if root.role == line[1]:                    
-                    print(line[1])
-                    print(line[0])
+            if count != 0:                
+                print_tree(root)
+                if root.role == line[1]:
                     print('the thing we were looking for was the root')
                     root = build_tree(Node(line[1]),root)
                     needToCheck.remove(line)
                 else:
                     print('its not the root')
-                    print(line[1])
-                    print(line[0])
-                    print(root.role == line[1])
                     (node,inTree) = traverse_down(root,line[1])
                     print(inTree)
                     if inTree:
-                        build_tree(node,Node(line[0]))
+                        root = build_tree(node,Node(line[0]))
                         print('before need to check')
                         print_array(needToCheck)
                         needToCheck.remove(line)
@@ -74,18 +68,19 @@ def main():
                 print_tree(root)
     print_tree(root)
 def print_array(array):
+    print()
     for i in array:
         print()
         for b in i:
             print (b+" ",end='')
+    print()
 def print_tree(root):
     print()
+    print(root.role+" ->",end='')
     for nodes in root.ascendant:
-        print(root.role+" ->",end='')
         if nodes != None:
             print(nodes.role, end='')
         elif nodes.ascendant != None:
             print_tree(nodes)
-        print()
 if __name__ == "__main__":
   main()
