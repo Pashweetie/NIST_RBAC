b46d29dc62273b6d6f9c27d115a1f4111730b678
Tim Lucca <tim.lucca88@gmail.com>
2019-11-07 19:58:44 -0700

    some gross nests but thats how print formatting be

--- roles.py
+++ roles.py
@@ -47,12 +47,13 @@ def printTree(key, roles):
   for role in roles:
     if roles.get(role) == key:
       plist.append(role)
-  sys.stdout.write(f"{key} -> ")
-  for item in plist:
-    sys.stdout.write(f"{item}, ")
-  print()
-  for item in plist:
-    printTree(item, roles)
+  if len(plist) > 0:
+    sys.stdout.write(f"{key} -> ")
+    for item in plist:
+      sys.stdout.write(f"{item}, ")
+    print()
+    for item in plist:
+      printTree(item, roles)
 
 def main():
   while True:
@@ -73,8 +74,11 @@ def matrixControls(roles, matrix):
     for ascendant in roles[descendant]:
       if "control" not in matrix[ascendant][ascendant]:
         matrix[ascendant][ascendant].append("control")
-      matrix[descendant][ascendant].append("own")
+      # matrix[descendant][ascendant].append("own")
   return matrix
+
+# def matrixOwn(roles, matrix, val):
+
 
 def buildEmptyMatrix(roles, res):
   matrix = dict()
@@ -87,27 +91,44 @@ def buildEmptyMatrix(roles, res):
     matrix[role] = inner
   return matrix
 
-def roleMatrix(roles, res):
-  n = len(res)
-  m = len(roles)
-  print("-----"*6)
-  for role in roles:
-    sys.stdout.write("   ")
-    for i in range(n+m):
-      if i >= m:
-        sys.stdout.write(f"   {res[i-m]}")
-      else:
-        sys.stdout.write(f"   {roles[i]}")
-      if (i+1) % 5 == 0 or i+1 == n+m:
-        sys.stdout.write(f"\n{role}:\n   ")
-    print("-----"*5)
+# Old version of printing the role matrix
+# def roleMatrix(roles, res):
+#   n = len(res)
+#   m = len(roles)
+#   print("-----"*6)
+#   for role in roles:
+#     sys.stdout.write("   ")
+#     for i in range(n+m):
+#       if i >= m:
+#         sys.stdout.write(f"   {res[i-m]}")
+#       else:
+#         sys.stdout.write(f"   {roles[i]}")
+#       if (i+1) % 5 == 0 or i+1 == n+m:
+#         sys.stdout.write(f"\n{role}:\n   ")
+#     print("-----"*5)
 
-def printMatrix(cmat):
-  for role in cmat:
-    print(f"\nRole {role}:")
-    for object in cmat[role]:
-      sys.stdout.write(f"{object} permissions: ")
-      print(cmat[role][object])
+def printMatrix(mat):
+  print("--------"*6)
+  emptys = " "
+  for role in mat:
+    stack = []
+    n = len(mat[role])
+    i = 0
+    sys.stdout.write("   ")
+    for object in mat[role]:
+      stack.append(object)
+      sys.stdout.write(f"{emptys*(9-len(object))}{object}")
+      if (i+1) % 5 == 0 or i+1 == n:
+        sys.stdout.write(f"\n{role}:")
+        for p in stack:
+          if mat[role][p] == []:
+            sys.stdout.write(emptys*9)
+          else:
+            sys.stdout.write(f"{emptys*(9-len(mat[role][p][0]))}{mat[role][p][0]}")
+        stack = []
+        sys.stdout.write("\n\n   ")
+      i = i+1
+    print("--------"*6)
 
 def checkDupes(r):
   c = []
@@ -125,21 +146,20 @@ def getResources(name):
   return line.split(" ")
 
 def resources(rroles, reverse_roles):
+  print(reverse_roles)
   roles = []
   for i in rroles:
     if i not in roles:
       roles.append(i)
     if rroles.get(i) not in roles:
       roles.append(rroles.get(i))
-
-  print(roles)
   while True:
     r = getResources("resourceObjects.txt")
     r = removeEmpty(r)
     (dupe, found) = checkDupes(r)
     if not found:
-      roleMatrix(roles,r)
-      cmat = matrixControls(reverse_roles, buildEmptyMatrix(roles,r))
+      mat = buildEmptyMatrix(roles, r)
+      cmat = matrixControls(reverse_roles, mat)
       printMatrix(cmat)
       break
     input(f"Duplicate object is found {dupe}, press ENTER to read it again")
